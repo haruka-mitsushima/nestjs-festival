b86786501ac13da03ec228944b167f91f7962b9a
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-25 15:23:42 +0900

    ユーザー登録機能の修正

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/createUser-dto';
 import { UserService } from './user.service';
 
 @Controller('api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  async createUser(@Body() dto: CreateUserDto): Promise<{ message: string }> {
+    return await this.userService.createUser(dto);
+  }
+
+  @Post('mailConditions')
+  async mailConditions(
+    @Body('mailAddress') mailAddress: string,
+  ): Promise<{ result: boolean; message?: string }> {
+    return await this.userService.mailConditions(mailAddress);
+  }
 
   @Post()
   async getUserById(@Body('userId') id: string): Promise<User> {
